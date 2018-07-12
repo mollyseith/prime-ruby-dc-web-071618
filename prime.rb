48bbec658ad8f8cfba8afac58593864974a445dc
Molly Seither <molly.seither@centre.edu>
2018-07-12 15:22:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,16 @@
 def prime?(number)
-#  start = 0
-#  bool = false
-#  len = number-1
-#  array=*(2..len)
-#  loop do
-#    if number % array[start] != 0
-#      start += 1
-#    else
-#      break
-#      bool = true
-#    end
-#  return bool
-#  end
-  if number%2 !=0 && number!=2
-    return true
-  else
-    return false
+  start = 0
+  bool = false
+  len = number-1
+  array=*(2..len)
+  loop do
+    if number % array[start] != 0
+      start += 1
+    else
+      break
+      bool = true
+    end
+  return bool
   end
 end
+
