cd5060e2dcb2433f0d08049b2d4e241e50e372b7
Molly Seither <molly.seither@centre.edu>
2018-07-12 15:28:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(number)
   return false if number < 2
   (2..number - 1).each do |x|
-      if (number % x) == 0
-          return false
-      end
+    if (number % x) == 0
+      return false
+    end
   end
-  true
+  return true
 end
