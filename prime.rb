5794f2d052d1592623263b30cccec1a3148192bf
Molly Seither <molly.seither@centre.edu>
2018-07-12 15:17:36 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime?(number)
-#create array 2 through number
-#iterate through, if
   start = 0
   bool = false
-  array=*(2..number-1)
+  len = number-1
+  array=*(2..len)
   loop do
     if number % array[start] != 0
       start += 1
