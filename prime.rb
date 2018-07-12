0277d767af41bcb5e12bc6f59a9389532ae285a5
Molly Seither <molly.seither@centre.edu>
2018-07-12 15:08:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,22 @@
 def prime?(number)
   idx = 1
 	result = nil
-	counter = 0 
-	
+	counter = 0
 
-	while idx <= number 
+
+	while idx <= number
 	if number % idx == 0 
 		counter = counter + 1
 	end
 	idx = idx + 1
 	end
-	
-	if counter > 2 
-		result = false 
-	else 
-		result = true 
+
+	if counter > 2
+		result = false
+	else
+		result = true
 	end
-	
+
 	return result
-	
+
 end
