37b51f1372a602a8015730635d7e2fdcf575ddd9
Molly Seither <molly.seither@centre.edu>
2018-07-12 15:26:24 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,4 @@
 def prime?(number)
-  start = 0
-  bool = false
-  len = number-1
-  array=*(2..len)
-  loop do
-    if number % array[start] != 0
-      start += 1
-    else
-      break
-      bool = true
-    end
-  return bool
-  end
+  (2..integer - 1).each {|x| return false if (integer % x) == 0 }
+  true
 end
