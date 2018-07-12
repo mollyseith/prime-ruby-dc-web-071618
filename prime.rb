b785e442f3634ae54dd2e27f860c064d3dce2ee2
Molly Seither <molly.seither@centre.edu>
2018-07-12 15:03:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,7 @@
 def prime?(number)
-  #array of numbers
-  #for each in array, besides 1 and number, if number% each == 0, return true
   for i in 2..(n-1)
     if n%i == 0
       return true
     end
   end
+end
